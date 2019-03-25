228681aa8eda88f00cde2563e422e1e21425cc7c
某亚瑟 <mouyase@qq.com>
2019-03-25 19:05:51 +0800

    2019年3月25日19:05:46

--- app/src/main/java/moe/yonjigen/roundtable/MainActivity.java
+++ app/src/main/java/moe/yonjigen/roundtable/MainActivity.java
@@ -1,7 +1,7 @@
 package moe.yonjigen.roundtable;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
