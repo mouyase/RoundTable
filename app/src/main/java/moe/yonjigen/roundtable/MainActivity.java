03394a77654a5f1eb58f0a3532c90d669fc5a3d2
某亚瑟 <mouyase@qq.com>
2019-03-25 09:37:38 +0800

    1

--- app/src/main/java/moe/yonjigen/roundtable/MainActivity.java
+++ app/src/main/java/moe/yonjigen/roundtable/MainActivity.java
@@ -1,7 +1,16 @@
 package moe.yonjigen.roundtable;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //1.创建OkHttpClient对象
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().connectTimeout(100, TimeUnit.SECONDS)//设置连接超时时间
+                .readTimeout(200, TimeUnit.SECONDS)//设置读取超时时间
+                .build();
+        //2.创建Request对象，设置一个url地址（百度地址）,设置请求方式。
+        Request request = new Request.Builder().url("https://yande.re.4cy.me/post.json?limit=1&login=mouyase&password_hash=7191ab9f615dfcf87680d11ab105edf9a68f5975").method("GET", null).build();
+        //3.创建一个call对象,参数就是Request请求对象
+        Call call = okHttpClient.newCall(request);
+        //4.请求加入调度，重写回调方法
+        call.enqueue(new Callback() {
+            //请求失败执行的方法
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            //请求成功执行的方法
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                System.out.println(response.body().string());
+            }
+        });
     }
 }
---
+++ app/src/main/java/moe/yonjigen/roundtable/RoundTableData.java
@@ -0,0 +1,7 @@
+package moe.yonjigen.roundtable;
+
+public class RoundTableData {
+    public static final String API_POST = "/post.json";
+    public static final String URL_HOST = "https://yande.re";
+    public static final String URL_POST = URL_HOST + API_POST;
+}
